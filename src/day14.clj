0402c60f169dee501ce2a9ad91c1d63513953569
Eike Spang <eike.spang@gmail.com>
2021-08-29 11:37:27 +0100

    cleanup d14

--- src/day14.clj
+++ src/day14.clj
@@ -9,31 +9,6 @@
                          .reset
                          (.update (.getBytes s)))))))
 
-(defn md52 [^String s]
-  (let [algorithm (java.security.MessageDigest/getInstance "MD5")
-        raw (.digest algorithm (.getBytes s))]
-    (format "%032x" (BigInteger. 1 raw))))
-
-(defn do-md5 [times ^String s]
-  (let [algorithm (java.security.MessageDigest/getInstance "MD5")
-        val       (format "%032x" (BigInteger. 1
-                                               (.digest algorithm (.getBytes s))))]
-    (loop [v val
-           i 0]
-      (if (= i times)
-        v
-        (recur (format "%032x" (BigInteger. 1
-                                            (.digest (doto algorithm
-                                                       .reset
-                                                       (.update (.getBytes v))))))
-               (inc i))))))
-
-(comment
-  (time (md52 "abc0"))
-  (time (md5 "abc0"))
-  (time (do-md5 2016 "abc0"))
-  (time (stretch-md5 2016 "abc0")))
-
 (defn first-triple [s]
   (loop [current (first s)
          s       (rest s)
@@ -57,8 +32,6 @@
         (if (= current (first s))
           (recur current (rest s) (inc cnt))
           (recur (first s) (rest s) 1))))))
-
-(def elements (seq "abcdef0123456789"))
 
 (defn find-quintuple [index salt c]
   (loop [i index]
@@ -88,10 +61,29 @@
   (loop [n 0
          s s]
     (if (= n times)
-      (md52 s)
+      (md5 s)
       (recur (inc n) (md5 s)))))
 
-(defn find-all-quintuples [s]
+(defn do-md5 [times ^String s]
+  (let [algorithm (java.security.MessageDigest/getInstance "MD5")
+        val       (format "%032x" (BigInteger. 1
+                                               (.digest algorithm (.getBytes s))))]
+    (loop [v val
+           i 0]
+      (if (= i times)
+        v
+        (recur (format "%032x" (BigInteger. 1
+                                            (.digest (doto algorithm
+                                                       .reset
+                                                       (.update (.getBytes v))))))
+               (inc i))))))
+
+
+(defn find-all-quintuples
+  "Finds all characters that occur at least 5 times in a row
+  in the given string. Returns a set with all characters found
+  or an empty set if none such character exist."
+  [s]
   (loop [current    (first s)
          s          (rest s)
          cnt        1
@@ -102,8 +94,11 @@
         (let [cnt' (if (= current (first s)) (inc cnt) 1)]
           (recur (first s) (rest s) cnt' quintuples'))))))
 
-
-(defn find-key [hashes index c]
+(defn find-key
+  "Returns all indexes of hashes that have a first tripe of
+  c in the last 1000 indexes. Looks up such triples in the
+  hashes map."
+  [hashes index c]
   (loop [i    (dec index)
          keys #{}]
     (if (or (<= i 0) (< i (- index 1000)))
@@ -112,10 +107,20 @@
         (recur (dec i) (if (= triple c) (conj keys i) keys))
         (recur (dec i) keys)))))
 
-(defn el64 [ss]
+(defn el64
+  "returns the 64th element of a sorted-set"
+  [ss]
   (first (drop 63 ss)))
 
-(defn find-nth2 [n salt]
+(defn find-nth2
+  "Searches for all keys defined by the rules of day14 y2016.
+  It first find the first triple and memoizes those hashes.
+  In case a triple exists it searches for all quintuples.
+  For each quintuples it searches in the memoized triples if
+  any valid keys can be found.
+  It can stop once it reaches an index that is 1000 bigger than
+  the 'n'th"
+  [n salt]
   (loop [hashes {}
          index  0
          keys   (sorted-set)]
@@ -135,19 +140,22 @@
             keys'      (apply conj keys (disj keys' -1))]
         (recur hashes' (inc index) keys')))))
 
-(defn make-hashes [n salt]
+(defn make-hashes
+  "creates a table of n hashes for the given salt."
+  [n salt]
   (into {} (for [i (range n)]
              [i (do-md5 2016 (str salt i))])))
 
 (comment
   ;part 2
-  ;; strech-md5: 40 secs to create 1000 hashes. Need ~23000 -> 8 minutes
-  ;; do-md5: 4 secs to create 1000 hashes
+  ;; strech-md5: 40 secs to create 1000 hashes. ~920s for 23k
+  ;; do-md5:      4 secs to create 1000 hashes. ~ 92s for 23k
   (time (def htabc (make-hashes 1000 "abc")))
   (stretch-md5 2016 "abc0")
   (find-all-quintuples "baaaacccccdeeeee")
   (time (def keys-test (find-nth2 64 "abc")))
   (el64 keys-test)
+  ; 98649.082242 msecs (~94% of the time is spend in creating the hashes)
   (time (def keys-p2 (find-nth2 64 "ahsbgdzn")))
   (el64 keys-p2)
   ,)
